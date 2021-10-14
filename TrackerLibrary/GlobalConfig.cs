2a0a52480f8ce799d7cfd9e8fe3abc5154e63f58
Alin Apostol <alin.cristian977@gmail.com>
2021-10-14 20:37:37 +0300

    Created Connections classes & Added CreatePrize mock code

---
+++ TrackerLibrary/GlobalConfig.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+namespace TrackerLibrary
+{
+    public static class GlobalConfig
+    {
+        public static List<IDataConnection> Connections { get; private set; } = new List<IDataConnection>();
+
+        public static void InitializeConnections(bool database, bool textFiles)
+        {
+            if (database)
+            {
+                //TODO - Set up the SQL connection properly
+                var sql = new SqlConnection();
+                Connections.Add(sql);
+            }
+
+            if (textFiles)
+            {
+                //TODO - Create the text connection
+                var text = new TextConnection();
+                Connections.Add(text);
+            }
+        }
+    }
+}
---
+++ TrackerLibrary/TextConnection.cs
@@ -0,0 +1,18 @@
+﻿namespace TrackerLibrary
+{
+    public class TextConnection : IDataConnection
+    {
+        //TODO - Wire up this method for text files.
+        /// <summary>
+        /// Saves a new prize to the text file.
+        /// </summary>
+        /// <param name="model">The prize information.</param>
+        /// <returns>The prize information, including the unique identifier.</returns>
+        public PrizeModel CreatePrize(PrizeModel model)
+        {
+            model.Id = 1;
+
+            return model;
+        }
+    }
+}
